b9ad50ceee9628a4f3f78839f25d875044f3ba1a
Henrique <henriqueos.92@outlook.com>
2025-03-13 09:45:54 -0300

    commit correção corrompimento do banco mysql

--- phpMyAdmin/config.inc.php
+++ phpMyAdmin/config.inc.php
@@ -18,7 +18,7 @@ $i++;
 /* Authentication type and info */
 $cfg['Servers'][$i]['auth_type'] = 'config';
 $cfg['Servers'][$i]['user'] = 'root';
-$cfg['Servers'][$i]['password'] = 'aZsh1p@2025';
+$cfg['Servers'][$i]['password'] = '';
 $cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 $cfg['Lang'] = '';
